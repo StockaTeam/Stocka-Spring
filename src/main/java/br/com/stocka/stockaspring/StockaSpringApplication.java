da86055a148e41b73f13a556b4e396bb949da6e4
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-03-15 09:48:35 -0300

    projeto base spring boot

---
+++ src/main/java/br/com/stocka/stockaspring/StockaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.stocka.stockaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockaSpringApplication.class, args);
+	}
+
+}
