9a5fdf9fa49969673a099acc8c3885a15dd341ed
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-04-13 23:48:59 -0300

    feat{Roles}: criação da classe Role Model Model com Autorização e enum Role Name

---
+++ src/main/java/br/com/stocka/stockaspring/enums/RoleName.java
@@ -0,0 +1,7 @@
+package br.com.stocka.stockaspring.enums;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_USER;
+}
+
---
+++ src/main/java/br/com/stocka/stockaspring/model/RoleModel.java
@@ -0,0 +1,51 @@
+package br.com.stocka.stockaspring.model;
+
+import java.io.Serializable;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import br.com.stocka.stockaspring.enums.RoleName;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_ROLE")
+public class RoleModel implements GrantedAuthority, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long roleId;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName roleName;
+
+    @Override
+    public String getAuthority() {
+        return this.roleName.toString();
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+}
+
