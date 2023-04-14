dd5998e62a914ebe40b924a348fc7e7d524aada5
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-04-13 23:38:03 -0300

    feat{UserController}: criação da classe User Controller com métodos CRUD e verificação de usuário já existente

---
+++ src/main/java/br/com/stocka/stockaspring/controller/UserController.java
@@ -0,0 +1,89 @@
+package br.com.stocka.stockaspring.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.stocka.stockaspring.dto.UserDto;
+import br.com.stocka.stockaspring.model.UserModel;
+import br.com.stocka.stockaspring.service.UserServiceImpl;
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/users")
+public class UserController {
+    
+    final UserServiceImpl userServiceImpl;
+
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
+    }
+
+    // @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PostMapping
+    public ResponseEntity<Object> saveUser(@RequestBody @Valid UserDto userDto){
+        if(userServiceImpl.existsByUsername(userDto.getUsername())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Username " + userDto.getUsername() + " is already in use!");
+        }
+        
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+        // userModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(userServiceImpl.save(userModel));
+    }
+
+    // @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers(){
+        return ResponseEntity.status(HttpStatus.OK).body(userServiceImpl.findAll());
+    }
+
+    // @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "id") Long id){
+        Optional<UserModel> userModelOptional = userServiceImpl.findById(id);
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+    }
+
+    // @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "id") Long id){
+        Optional<UserModel> userModelOptional = userServiceImpl.findById(id);
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        userServiceImpl.delete(userModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully.");
+    }
+
+    // @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "id") Long id,
+                                                    @RequestBody @Valid UserDto userDto){
+        Optional<UserModel> userModelOptional = userServiceImpl.findById(id);
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+        userModel.setUserId(userModelOptional.get().getUserId());
+        // userModel.setRegistrationDate(userModelOptional.get().getRegistrationDate());
+        return ResponseEntity.status(HttpStatus.OK).body(userServiceImpl.save(userModel));
+    }
+}
