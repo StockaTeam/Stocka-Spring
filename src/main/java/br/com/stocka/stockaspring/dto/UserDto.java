6f0e856e95fa06df52e70f99cfbe26d048179e39
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-04-15 12:22:34 -0300

    feat{UserDto}: adição de mensagens de validação dos campos

--- src/main/java/br/com/stocka/stockaspring/dto/UserDto.java
+++ src/main/java/br/com/stocka/stockaspring/dto/UserDto.java
@@ -1,17 +1,26 @@
 package br.com.stocka.stockaspring.dto;
 
+import java.time.LocalDateTime;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class UserDto {
     
     @NotBlank
-    @Size(min = 3, max = 40)
+    @Size(min = 3, max = 40, message = "username must to be between 3 and 40 characters")
     private String username;
     @NotBlank
-    @Size(min = 6, max = 20)
+    @Size(min = 6, max = 20, message = "password must to be between 6 and 20 characters")
     private String password;
+    private LocalDateTime registrationDate;
     
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
     public String getUsername() {
         return username;
     }
